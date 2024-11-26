0c86ecad7a035b59d6d3b5151abe9e4fa789b19a
MJAYANTH6360 <mjayanth683@gmail.com>
2024-11-26 00:32:39 -0800

    Create if else.c

---
+++ if else.c
@@ -0,0 +1,10 @@
+int marks = 75;
+if (marks >= 90) {
+    printf("Grade A\n");
+} else if (marks >= 80) {
+    printf("Grade B\n");
+} else if (marks >= 70) {
+    printf("Grade C\n");
+} else {
+    printf("Grade D\n");
+}
