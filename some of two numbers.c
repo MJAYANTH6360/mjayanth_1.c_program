24594c6965073c834d2e0b256534432ea203b568
MJAYANTH6360 <mjayanth683@gmail.com>
2024-11-25 23:19:35 -0800

    Create some of two numbers.c

---
+++ some of two numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("entrer the value of principal,rateand time\n")
+float principal,rate,time,si;
+scanf("%f%f%f",&principal,&rate,&time);
+si=(p*r*t)/100;
+printf("the simple interest is %f",si);
+'return0';
+}
