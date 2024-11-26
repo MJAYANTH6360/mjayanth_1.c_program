da2d063ae89b671f6cf98f6a3a6fe65ccc30158f
MJAYANTH6360 <mjayanth683@gmail.com>
2024-11-26 00:10:16 -0800

    Create else if statement.c

---
+++ else if statement.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){ int marts printf ("Enter the marts"),
+
+Scanf (\% P ,f mas)
+
+EF (marks S = 90 )\
+
+printf('Grade, A ''M")",
+
+}else if (marks > = 75 ),\
+
+printf ("borade, Blh"); }
+else if (marks 7 =50^ prime prime ) {
+
+printf(grade, c\n");
+
+} else {
+printf('grade,f\n");
+}
+return 0;
+}
