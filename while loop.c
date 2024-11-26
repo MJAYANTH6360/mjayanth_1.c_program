573d821cc7fd763fc927f0e172e2bc80358f7e25
MJAYANTH6360 <mjayanth683@gmail.com>
2024-11-26 01:18:16 -0800

    Create while loop.c

---
+++ while loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int count = 1;  // Initialize the counter variable
+
+    while (count <= 5) {  // Condition to check before each iteration
+        printf("%d\n", count);  // Print the current value of count
+        count++;  // Increment the counter
+    }
+
+    return 0;  // Exit the program
+}
