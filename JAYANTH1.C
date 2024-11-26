ba7500de4a2f2640717ca0492e2da84977c8ae2a
MJAYANTH6360 <mjayanth683@gmail.com>
2024-11-25 22:56:24 -0800

    Add files via upload

---
+++ JAYANTH1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num;
+{
+printf("enter an integer");
+scanf("%d",&num);
+if(num>=0)
+printf("the number is positive.\n");
+else
+printf("the number is negative.\n");
+}
+return 0;
+}
